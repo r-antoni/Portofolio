376c5e14d0fe7434aa197d0a5dd37a153b359929
r-antoni <reindraantoni@gmail.com>
2023-08-31 08:29:55 +0700

    Code format

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,15 +15,15 @@ const Contact = (props: Props) => {
       <BackgroundCircles />
       <div className="flex flex-col items-center gap-5 z-50 tracking-wide">
         <p className="text-2xl text-greentext font-semibold flex items-center tracking-wide">
-        04.<span className="text-white"> What's Next ?</span>
-      </p>
-      <h2 className="text-4xl font-semibold text-greentext">Get In Touch</h2>
-      <p className="max-w-[600px] text-center text-body">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto nostrum accusamus at porro aliquam nisi
-        facilis tempora nihil adipisci? Totam omnis laudantium necessitatibus sint eligendi repudiandae, sunt quasi
-        voluptas dicta.
-      </p>
-      <HelloButton />
+          04.<span className="text-white"> What's Next ?</span>
+        </p>
+        <h2 className="text-4xl font-semibold text-greentext">Get In Touch</h2>
+        <p className="max-w-[600px] text-center text-body">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto nostrum accusamus at porro aliquam nisi
+          facilis tempora nihil adipisci? Totam omnis laudantium necessitatibus sint eligendi repudiandae, sunt quasi
+          voluptas dicta.
+        </p>
+        <HelloButton />
       </div>
       <div>
         <Social />
--- src/components/Contact/components/HelloButton.tsx
+++ src/components/Contact/components/HelloButton.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const HelloButton = (props: Props) => {
   return (
     <a href="mailto:reindraantoni@gmail.com">
-        <button className="w-40 h-16 border z-50 border-greentext font-semibold mt-10 tracking-widest rounded-md text-white hover:bg-greentext hover:text-background duration-300">
-          Say Hello
-        </button>
-      </a>
-  )
-}
+      <button className="w-40 h-16 border z-50 border-greentext font-semibold mt-10 tracking-widest rounded-md text-white hover:bg-greentext hover:text-background duration-300">
+        Say Hello
+      </button>
+    </a>
+  );
+};
 
-export default HelloButton
+export default HelloButton;
