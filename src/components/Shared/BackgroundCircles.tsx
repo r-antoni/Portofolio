b55f5cca15b562c68915822a4174d687b39dfb28
r-antoni <reindraantoni@gmail.com>
2023-09-04 20:28:38 +0700

    .

--- src/components/Shared/BackgroundCircles.tsx
+++ src/components/Shared/BackgroundCircles.tsx
@@ -18,8 +18,8 @@ const BackgroundCircles = (props: Props) => {
       }}
       className="relative flex justify-center items-center z-0"
     >
-      <div className="absolute hidden md:inline-flex opacity-80 border-greentext border-2 rounded-full md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[400px] xxl:w-[500px] xxl:h-[500px] md:mt-[25rem] animate-ping" />
-      <div className="absolute hidden md:inline-flex opacity-20 border-greentext border-[3px] rounded-full md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[800px] xxl:w-[1000px] xxl:h-[1000px] md:mt-[25rem] animate-pulse" />
+      <div className="absolute hidden md:inline-flex opacity-80 border-greentext border-2 rounded-full md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[400px] xxl:w-[450px] xxl:h-[450px] md:mt-[25rem] animate-ping" />
+      <div className="absolute hidden md:inline-flex opacity-20 border-greentext border-[3px] rounded-full md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[800px] xxl:w-[950px] xxl:h-[950px] md:mt-[25rem] animate-pulse" />
     </motion.div>
   );
 };
