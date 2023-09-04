2cf4ebb64aeaf066fb0df6cf0ab5303c94102ce4
r-antoni <reindraantoni@gmail.com>
2023-09-04 14:33:57 +0700

    Skills icon small update

--- src/components/About/components/Skills.tsx
+++ src/components/About/components/Skills.tsx
@@ -12,39 +12,39 @@ const Skills = (props: Props) => {
       </p>
       <ul className="grid grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-x-40 mt-5 text-sm px-5 text-body">
         <li className="flex items-center gap-3">
-          <Image src={js} alt="Javascript Logo" className="w-8 rounded-lg"/>
+          <Image src={js} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Javascript (ES6+)
         </li>
         <li className="flex items-center gap-3">
-        <Image src={react} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={react} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           React.js
         </li>
         <li className="flex items-center gap-3">
-        <Image src={next} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={next} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Next.js
         </li>
         <li className="flex items-center gap-3">
-        <Image src={tailwind} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={tailwind} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           TailwindCSS
         </li>
         <li className="flex items-center gap-3">
-        <Image src={ts} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={ts} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Typescript
         </li>
         <li className="flex items-center gap-3">
-        <Image src={nodejs} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={nodejs} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Node.js
         </li>
         <li className="flex items-center gap-3">
-        <Image src={vite} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={vite} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Vite
         </li>
         <li className="flex items-center gap-3">
-        <Image src={netlify} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={netlify} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Netlify
         </li>
         <li className="flex items-center gap-3">
-        <Image src={git} alt="Javascript Logo" className="w-8 rounded-lg"/>
+        <Image src={git} alt="Javascript Logo" className="w-6 lg:w-8 rounded-lg"/>
           Github
         </li>
       </ul>
